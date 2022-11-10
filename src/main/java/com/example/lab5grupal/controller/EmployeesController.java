602ffbd5bbf9084c36aa6c6bb08a74d6e8da71a0
joelsilvera <joel.silvera@pucp.edu.pe>
2022-11-09 19:20:57 -0500

    asc, desc

--- src/main/java/com/example/lab5grupal/controller/EmployeesController.java
+++ src/main/java/com/example/lab5grupal/controller/EmployeesController.java
@@ -15,9 +15,21 @@ public class EmployeesController {
     EmployeeRepository employeeRepository;
 
     //LISTA EMPLEADOS
-    @GetMapping(value = {"", "/", "lista"})
+    @GetMapping(value = {"", "lista"})
     public String listEmployee(Model model){
         model.addAttribute("listaEmpleados", employeeRepository.findAll());
+        return "employee/lista";
+    }
+    //LISTA EMPLEADOS ASCENDENTE (De la A a la Z)
+    @GetMapping("/listaAscendente")
+    public String listEmployeeAsc(Model model){
+        model.addAttribute("listaEmpleados", employeeRepository.obtenerListaAscendente());
+        return "employee/lista";
+    }
+    //LISTA EMPLEADOS DESCENDENTE (De la Z a la A)
+    @GetMapping("/listaDescendente")
+    public String listEmployeeDes(Model model){
+        model.addAttribute("listaEmpleados", employeeRepository.obtenerListaDescendente());
         return "employee/lista";
     }
 }
--- src/main/java/com/example/lab5grupal/repository/EmployeeRepository.java
+++ src/main/java/com/example/lab5grupal/repository/EmployeeRepository.java
@@ -2,7 +2,15 @@ package com.example.lab5grupal.repository;
 
 import com.example.lab5grupal.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
+    @Query(nativeQuery = true, value = "select * from employees order by first_name")
+    List<Employee> obtenerListaAscendente();
+
+    @Query(nativeQuery = true, value = "select * from employees order by first_name desc")
+    List<Employee> obtenerListaDescendente();
 }
